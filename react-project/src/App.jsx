48cde984aec81ee98faf328cc72280e07cc27249
AbderrahmaneChaffa <Ab.chaffa31@gmail.com>
2026-03-01 11:55:01 +0100

    `Added Header and Main components, updated App component to use them`

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-
+function Header({ name, year }) {
   return (
-   
+    <header>
+      <h1>My {name} React App</h1>
+      <p>Copyright © {year}</p>
+    </header>
+  );
+}
+const items = [
+  "Macaroni and cheese",
+  "salmon with Potatoes",
+  "chicken with rice",
+];
+function Main({ dishes }) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li style={{ listStyleType: "none" }}>{dish}</li>
+      ))}
+    </ul>
+  );
+}
+function App() {
+  return (
+    <div>
+      <Header name="Abderrahmane" year={new Date().getFullYear()} />
+      <Main dishes={items} />
+    </div>
   );
 }
 
-export default App
+export default App;
